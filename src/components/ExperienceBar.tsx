d86e76f665d70f69451c25c8bcd9224de9585854
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2021-02-25 20:33:30 -0300

    feat: commit inicial

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
+export function ExperienceBar() {
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }}></div>
+
+        <span className={styles.currentExperience}>300 xp</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
