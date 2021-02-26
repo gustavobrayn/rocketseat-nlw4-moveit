e838e6dc67c2e91db196206750cceec3c6118528
Gustavo Brayn <gustavo.augusto.brayn@gmail.com>
2021-02-25 21:24:10 -0300

    feat: adicionado componentes a tela inicial, favicon e titulo da pagina
    Componentes adicionados:
      -CompletedChallenges
      -Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import Head from "next/head";
+
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
@@ -6,11 +10,16 @@ import styles from "../styles/pages/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
